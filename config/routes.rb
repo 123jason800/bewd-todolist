63041f25afeeaad0323f070d69b8a54e954976a3
root <root@DESKTOP-E81SO17.localdomain>
2021-01-26 23:29:17 -0800

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'homepage#index'
 
   # Add routes below this line
-
-
-
-
+  get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
+  delete 'tasks/:id' => 'tasks#destroy'
+  put '/tasks/:id/mark_active' => 'tasks#mark_active'
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
 
   # Add routes below above line
 
